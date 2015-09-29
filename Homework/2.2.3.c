346e83ba199cbcf6452607b83523fdfe199e41e5
Spencer Zhang <spencerpomme@live.com>
2015-09-29 17:02:37 +0800

    SEP29

---
+++ Homework/2.2.3.c
@@ -0,0 +1,17 @@
+/* exercise 2.2.3.c */
+struct rectangle {
+    double height;
+    double width;
+};
+
+struct triangle {
+    double base;
+    double height;
+    double angle1;
+    double angle2;
+    double angle3;
+};
+
+struct circle {
+    double radius;
+};

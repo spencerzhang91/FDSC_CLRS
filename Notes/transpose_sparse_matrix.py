4b401685c9c654277ede8049bb88fbd72d1cb400
Spencer Zhang <spencerpomme@live.com>
2015-10-08 20:07:09 +0800

    ...

--- Notes/transpose_sparse_matrix.py
+++ Notes/transpose_sparse_matrix.py
@@ -1,7 +1,7 @@
 # Define a class to store sparse matrix elements
 class Element:
     '''
-    A class represent a non-zero element of a sparse matrix
+    A class represent a non-zero element of a sparse matrix.
     '''
     def __init__(self, row=0, col=0, val=0):
         self.row = row                      # row number of element
@@ -19,36 +19,64 @@ class Element:
 
 def transpose_sparse(matrix: list)->list:
     '''
-    A function to transpose sparse matrix
-    This function does not preserve an order according to row or col
+    A function to transpose sparse matrix.
+    This function does not preserve an order according to row or col.
     '''
     info = Element()
     info.row = matrix[0].col
     info.col = matrix[0].row
     info.val = matrix[0].val
     new_matrix = [info]
-    for i in range(1,len(matrix)):
-        new_matrix.append(Element(matrix[i].col,
-                                  matrix[i].row,
-                                  matrix[i].val))
+    if info.val:
+        for i in range(1,len(matrix)):
+            new_matrix.append(Element(matrix[i].col,
+                                      matrix[i].row,
+                                      matrix[i].val))
     return new_matrix
 
 
 def transpose_ordered(matrix: list)->list:
     '''
-    This function will sort matrix list elements according to row number
+    This function will sort matrix list elements according to row number.
     '''
     info = Element(matrix[0].col, matrix[0].row, matrix[0].val)
     new_matrix = [info]
-    for i in range(matrix[0].col):
-        for j in range(1, matrix[0].val + 1):
-            if matrix[j].col == i:
-                new_matrix.append(Element(matrix[j].col,
-                                          matrix[j].row,
-                                          matrix[j].val))
+    if info.val:
+        for i in range(matrix[0].col):
+            for j in range(1, matrix[0].val + 1):
+                if matrix[j].col == i:
+                    new_matrix.append(Element(matrix[j].col,
+                                              matrix[j].row,
+                                              matrix[j].val))
+    return new_matrix
+
+
+def transpose_fast(matrix: list)->list:
+    '''
+    This function can transpose in O(2n) time while the former need O(n^2)
+    worst time.
+    The main idea is that record non-zero element number in each colum of
+    the sparse matrix which indicates the position of elements in the new
+    matrix.
+    '''
+    info = Element(matrix[0].col, matrix[0].row, matrix[0].val)
+    new_matrix = [0] * (matrix[0].val + 1)  # info as [0] and other 3 vals as [1][2][3]
+    starting_pos = [0] * matrix[0].col
+    row_elements = [0] * matrix[0].col      # non-zero elements in origin's each col
+                                            # which means in new matrix's each row
+    new_matrix[0] = info
+    starting_pos[0] = 1                     # new matrix's first element is 'info' too
+    if info.val:
+        for i in range(1, matrix[0].val + 1):
+            row_elements[matrix[i].col] += 1
+        for i in range(1, matrix[0].col):
+            starting_pos[i] = starting_pos[i-1] + row_elements[i-1]
+        for i in range(1, matrix[0].val + 1):
+            j = starting_pos[matrix[i].col]
+            new_matrix[j] = Element(matrix[i].col, matrix[i].row, matrix[i].val)
     return new_matrix
         
-
+        
 
 if __name__ == '__main__':
     
@@ -58,6 +86,5 @@ if __name__ == '__main__':
     M3 = transpose_ordered(M1)
     print('M2:\n', M2)
     print('M3:\n', M3)
-    
-
-
+    M4 = transpose_fast(M1)
+    print('M4:\n', M4)

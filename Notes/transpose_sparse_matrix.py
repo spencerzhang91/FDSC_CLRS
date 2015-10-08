105ed832417d59add102814f391bb5ddd72bac6c
Spencer Zhang <spencerpomme@live.com>
2015-10-08 17:03:40 +0800

    ...

--- Notes/transpose_sparse_matrix.py
+++ Notes/transpose_sparse_matrix.py
@@ -4,9 +4,9 @@ class Element:
     A class represent a non-zero element of a sparse matrix
     '''
     def __init__(self, row=0, col=0, val=0):
-        self.row = row
-        self.col = col
-        self.val = val
+        self.row = row                      # row number of element
+        self.col = col                      # col number of element
+        self.val = val                      # value of element (non-zero)
 
     def __repr__(self):
         return "{(%d,%d): %d}" % (self.row, self.col, self.val)
@@ -17,9 +17,10 @@ class Element:
 # non-zero element number
 
 
-def transpose_sparse(matrix: list) -> list:
+def transpose_sparse(matrix: list)->list:
     '''
     A function to transpose sparse matrix
+    This function does not preserve an order according to row or col
     '''
     info = Element()
     info.row = matrix[0].col
@@ -33,14 +34,30 @@ def transpose_sparse(matrix: list) -> list:
     return new_matrix
 
 
+def transpose_ordered(matrix: list)->list:
+    '''
+    This function will sort matrix list elements according to row number
+    '''
+    info = Element(matrix[0].col, matrix[0].row, matrix[0].val)
+    new_matrix = [info]
+    for i in range(matrix[0].col):
+        for j in range(1, matrix[0].val + 1):
+            if matrix[j].col == i:
+                new_matrix.append(Element(matrix[j].col,
+                                          matrix[j].row,
+                                          matrix[j].val))
+    return new_matrix
+        
 
 
 if __name__ == '__main__':
     
-    M1 = [Element(6,8,3), Element(1,1,3), Element(4,2,-5), Element(5,5,2)]
-    print(M1)
+    M1 = [Element(6,8,3), Element(1,3,3), Element(3,2,-5), Element(4,5,2)]
+    print('M1:\n', M1)
     M2 = transpose_sparse(M1)
-    print(M2)
+    M3 = transpose_ordered(M1)
+    print('M2:\n', M2)
+    print('M3:\n', M3)
     
 
 

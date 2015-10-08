d7f433ce71464f00153309820d3f3a7d3eb79f9f
Spencer Zhang <spencerpomme@live.com>
2015-10-09 07:09:09 +0800

    ..

---
+++ Notes/simple_matrix_multiplication.py
@@ -0,0 +1,9 @@
+# simple matrix multiplication algorithm (none sparse matrix)
+# as a supplementary code for 2-9_MatrixMultiplication.c
+
+def mul_matrix(m1:list, m2:list)->list:
+    '''
+    Input of this function is two 2d list.
+    Output is a 2d list. If the result is a number, then return [[num]]
+    '''
+    

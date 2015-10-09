ea8779a1efcae287716968c17da147b2e21858fd
Spencer Zhang <spencerpomme@live.com>
2015-10-09 09:19:00 +0800

    OCT09

--- Notes/simple_matrix_multiplication.py
+++ Notes/simple_matrix_multiplication.py
@@ -1,9 +1,39 @@
 # simple matrix multiplication algorithm (none sparse matrix)
 # as a supplementary code for 2-9_MatrixMultiplication.c
 
+from matrix_transpose_basic import transpose                # import transpose module
+
 def mul_matrix(m1:list, m2:list)->list:
     '''
     Input of this function is two 2d list.
-    Output is a 2d list. If the result is a number, then return [[num]]
+    Output is a 2d list. If the result is a number,
+    then return [[num]].
+    Assume both matrixs are well structured (row & col)
     '''
+    if len(m1[0]) != len(m2):                               # verify valid multiplication
+        print("Invalid multiplication attempted.")
+        return None
+    nrow = len(m1)                                          # row number of product
+    ncol = len(m2[0])                                       # col number of product
+    product = []
+    temp = []
+    s = 0
+    m2 = transpose(m2)                                      # transpose for simplicity
+    for rowm1 in m1:
+        for rowm2 in m2:
+            for i in range(len(m1[0])):
+                s += rowm1[i] * rowm2[i]
+            temp.append(s)                                  # product's element
+            s = 0                                           # reset to zero in next loop
+        product.append(temp)                                # product's row
+        temp = []                                           # reset to empty list
+    return product
+
+
+if __name__ == '__main__':
+
+    M1 = [[1,1,1],[1,1,1]]
+    M2 = [[1,1],[1,1],[1,1]]
+    res = mul_matrix(M1, M2)
+    print(res)
     

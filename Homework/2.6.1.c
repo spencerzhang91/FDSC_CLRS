dde79f1daed39d3255dd0844979a3a8806b7967a
Spencer Zhang <spencerpomme@live.com>
2015-10-12 16:27:58 +0800

    OCT12

--- Homework/2.6.1.c
+++ Homework/2.6.1.c
@@ -1,6 +1,7 @@
 /* Excercise 2.6.1.c */
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include <ctype.h>
 #define MAX 128
 
@@ -10,23 +11,41 @@ typedef struct {
 } charcount;
 
 void count(char *string, int len);
+bool instr(char *string, char ch);
 
 int main(void)
 {
     char *chars = "To be or not to be, it's a question.";
     int len = strlen(chars);
     count(chars, len);
-    
     return 0;
 }
 
 void count(char *string, int len)
 {
-    char temp[(MAX > len)? len: MAX];
-    temp[0] = string[0];
-
-    for (int i = 1; i < len; i++)
+    if (len > 0)
     {
-        ;
+       char temp[MAX] = "";
+        temp[0] = string[0];
+        int uniqe = 1;
+        for (int i = 1; i < len; i++)
+        {
+            if (instr(temp, string[i]))
+                continue;
+            else
+        } 
     }
+    
+}
+
+bool instr(char *string, char ch)
+{
+    int i = 0;
+    while (string[i] != '\0')
+    {
+        if (string[i] == ch)
+            return true;
+        i++;
+    }    
+    return false;
 }

75eaa94ab9ecda335648ce5a8d1d07a3bd74d70e
Spencer Zhang <spencerpomme@live.com>
2015-10-12 16:59:52 +0800

    ...

--- Homework/2.6.1.py
+++ Homework/2.6.1.py
@@ -5,8 +5,11 @@ def count(string):
     for ch in string:
         if ch in freq:
             freq[ch] += 1
+    print(len(freq))
     return freq
 
 if __name__ == "__main__":
     s = "To be or not to be, it's a question."
-    print(count(s))
+    d = count(s)
+    for key in d.keys():
+        print(key, '->', d[key])

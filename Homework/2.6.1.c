8a5e0ab3c90349900b04587699c6b1675a64d209
Spencer Zhang <spencerpomme@live.com>
2015-10-12 18:28:39 +0800

    OCT12

--- Homework/2.6.1.c
+++ Homework/2.6.1.c
@@ -18,6 +18,7 @@ int main(void)
     char *chars = "To be or not to be, it's a question.";
     int len = strlen(chars);
     count(chars, len);
+    
     return 0;
 }
 
@@ -25,17 +26,43 @@ void count(char *string, int len)
 {
     if (len > 0)
     {
-       char temp[MAX] = "";
+        char temp[MAX] = "";
         temp[0] = string[0];
-        int uniqe = 1;
+        int unique = 1;
+        int duplic = 0;
+        
         for (int i = 1; i < len; i++)
         {
             if (instr(temp, string[i]))
+            {
+                duplic++;
                 continue;
+            }   
             else
-        } 
+            {
+                temp[i - duplic] = string[i];
+                unique++;
+            }     
+        }
+        printf("There are %d unique chars in this string.\n", unique);
+        charcount chars[unique];
+        
+        for (int i = 0; i < unique; i++)
+        {
+            chars[i].character = temp[i];
+            chars[i].frequency = 0;
+        }
+        for (int i = 0; i < len; i++)
+            for (int j = 0; j < unique; j++)
+            {
+                if (temp[j] == string[i])
+                    chars[j].frequency++;
+            }
+        for (int k = 0; k < unique; k++)
+            printf("%c: %d\n", chars[k].character, chars[k].frequency);
     }
-    
+    else
+        fprintf(stderr, "Empty string input.\n");    
 }
 
 bool instr(char *string, char ch)
@@ -49,3 +76,4 @@ bool instr(char *string, char ch)
     }    
     return false;
 }
+

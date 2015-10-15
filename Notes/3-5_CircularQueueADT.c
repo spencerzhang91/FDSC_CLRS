a319ffce356707c2e476ee00d74d46b51249aad3
Spencer Zhang <spencerpomme@live.com>
2015-10-15 17:09:29 +0800

    OCT15

--- Notes/3-5_CircularQueueADT.c
+++ Notes/3-5_CircularQueueADT.c
@@ -8,18 +8,27 @@ typedef struct {
 	// other fields
 } element;
 /* global defination of queue */
-element queue[MAX_QUEUE_SIZE];
+element queue[MAX_QUEUE_SIZE] = {0,0,0,0,0,0};
 int rear = 0;
 int front = 0;
 
-void showqueueu(element *queue);
+void showqueue(element *queue);
 void enqueue(int front, int *rear, element item);
 element dequeue(int *front, int rear);
 
 int main(void)
 {
 	element n1 = {1};
+	element n2 = {2};
+	element n3 = {3};
+	element n4 = {4};
+	element n5 = {5};
 	enqueue(front, &rear, n1);
+	enqueue(front, &rear, n2);
+	enqueue(front, &rear, n3);
+	enqueue(front, &rear, n4);
+	enqueue(front, &rear, n5);
+	showqueue(queue);
 	return 0;
 }
 
@@ -27,19 +36,23 @@ void enqueue(int front, int *rear, element item)
 {
 	*rear = (*rear + 1) % MAX_QUEUE_SIZE;
 	if (front == *rear)
-		return queue_full(rear);
+		printf("Queue is full.\n");
 	queue[*rear] = item;
 }
 
 element dequeue(int *front, int rear)
 {
 	if (*front == rear)
-		return queue_empty();
+		printf("Queue is empty.\n");
 	*front = (*front + 1) % MAX_QUEUE_SIZE;
 	return queue[*front];
 }
 
-void showqueueu(element *queue)
+void showqueue(element *queue)
 {
-	
+	if (rear < front)
+	   rear = rear + MAX_QUEUE_SIZE;
+	printf("front: %d, rear: %d\n", front, rear);
+	for (int i = front; i <= rear; i++)
+	   printf("%d ", queue[i]);
 }

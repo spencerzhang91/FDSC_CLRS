b4bdfc5d252de3ee75c1e5a8d7f7108313159aae
Spencer Zhang <spencerpomme@live.com>
2015-10-16 13:59:58 +0800

    OCT16

---
+++ Notes/Linked_QueueADT.c
@@ -0,0 +1,94 @@
+/* Linked list based Queue ADT implementation */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define ElementType int
+#define MAXSIZE 10
+
+typedef struct {
+	ElementType data;
+	Node *next;
+} Node;
+
+typedef struct {
+	Node *front, *rear;
+	int cursize = 0;
+	int maxsize;
+} Qnode;
+
+typedef Qnode *Queue;
+
+Queue CreateQueue(int maxsize);
+bool IsFull(Queue Q);
+bool IsEmpty(Queue Q);
+void enqueue(Queue Q, ElementType item);
+ElementType dequeue(Queue Q);
+
+int main(void) 									// test fucntion
+{
+	
+	return 0;
+}
+
+Queue CreateQueue(int maxsize)
+{
+	Queue temp = (Queue)malloc(sizeof(Qnode));
+	temp->front = temp->rear = NULL;
+	temp->maxsize = maxsize;
+	return temp;
+}
+
+bool IsFull(Queue Q)
+{
+	return Q->cursize == Q->maxsize;
+}
+
+bool IsEmpty(Queue Q)
+{
+	return Q->front == NULL;
+}
+
+void enqueue(Queue Q, ElementType item)
+{
+	Node *rearcell;
+	ElementType rearelem = item;
+	if (IsFull(Q))
+		printf("The queue is full.\n");
+	else
+	{
+		rearcell = (Node *)malloc(sizeof(Node));
+		rearcell->data = rearelem;
+		if (Q->cursize == 0)
+			Q->front = Q->rear = rearcell;
+		else
+		{
+			Q->rear->next = rearcell;
+			Q->rear = Q->rear->next;
+		}	
+		++Q->cursize;
+	}
+}
+
+ElementType dequeue(Queue Q)
+{
+	Node *frontcell;
+	ElementType frontelem;
+	if (IsEmpty(Q))
+	{
+		printf("The queue is empty.\n");
+		ElementType error = -1;
+		return error; 
+	}
+	else
+	{
+		frontcell = Q->front;
+		frontelem = frontcell->data;
+		if (Q->front == Q->rear)
+			Q->front = Q->rear = NULL;
+		else
+			Q->front = Q->front->next;
+		free(frontcell);
+		--Q->cursize;
+		return frontelem;
+	}
+}

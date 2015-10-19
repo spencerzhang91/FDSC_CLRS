3cc588a56808ad1e607bc27942f5c464580c988e
Spencer Zhang <spencerpomme@live.com>
2015-10-19 23:37:12 +0800

    OCT19

--- Homework/2.6.5.c
+++ Homework/2.6.5.c
@@ -8,11 +8,21 @@ int main(void)
 {
 	char *string = "abcdefg";
 	char ch = 'c';
+	char *ptrc;
+	ptrc = strch1(string, ch);
+	printf("%s", ptrc);
 	
 	return 0;
 }
 
 char *strch1(char *string, char ch)
 {
-    puts("haha");
+    int str_len = 0;
+    while (string != '\0')
+    {
+    	string++;
+    	if (*string == ch)
+    		return string;
+    }
+    return NULL;    
 }

39275708738a1d2771ae0fe47618614f87b04ffe
Spencer Zhang <spencerpomme@live.com>
2015-10-20 23:22:53 +0800

    OCT20

--- Homework/2.8.1.c
+++ Homework/2.8.1.c
@@ -1,3 +1,34 @@
 /* Additional Excerices of chapter 2 */
 /* Total 10 problems, 1 of ten*/
 /* Exercise 2.8.1.c */
+#include <stdio.h>
+#define ElementType int
+#define N 5
+
+void invert(ElementType *source, ElementType *target);
+void display(ElementType *array, int n);
+
+int main(void)
+{
+    ElementType list[N] = {1,2,3,4,5};
+    ElementType result[N];
+    invert(list, result);
+    display(list, N);
+    display(result, N);
+
+    return 0;
+}
+
+void invert(ElementType *source, ElementType *target)
+{
+    int j = N-1, i = 0;
+    for (; i < N; i++, j--)
+    	target[i] = source[j];
+}
+
+void display(ElementType *array, int n)
+{
+	for (int i = 0; i < n; i++)
+		printf("%d ", array[i]);
+	putchar('\n');
+}

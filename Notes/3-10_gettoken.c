97d36773513a8d462f1cf46dc95b30c0ac0f7ef9
Spencer Zhang <spencerpomme@live.com>
2015-10-21 10:07:31 +0800

    OCT21

---
+++ Notes/3-10_gettoken.c
@@ -0,0 +1,21 @@
+/* code list 3-10_gettoken.c */
+precedence get_token(char *symbol, int *n)
+{
+	/* get the next token, symbol is the 
+	character representaion, which is returned,
+	the token is represented by its enumerated
+	value, which is returned in the function name */
+	*symbol = expr[(*n)++];
+	switch (*symbol)
+	{
+		case '(': return lparen;
+		case ')': return rparen;
+        case '+': return plus;
+        case '-': return minus;
+        case '*': return times;
+        case '/': return divide;
+        case '%': return mod;
+        case ' ': return eos;
+        default : return operand;
+	}
+}

4ae5585e721d15c93ea55d7b22ca7fbe2cafa075
Spencer Zhang <spencerpomme@live.com>
2015-10-22 16:43:39 +0800

    ...

--- Notes/Linked_StackADT.c
+++ Notes/Linked_StackADT.c
@@ -6,14 +6,14 @@
 #define MAX_STACK_SIZE 100
 
 struct Node {
-	ElementType data;
-	struct Node *next;
+    ElementType data;
+    struct Node *next;
 };
 
 typedef struct {
-	struct Node *head;
-	int maxsize;
-	int cursize;
+    struct Node *head;
+    int maxsize;
+    int cursize;
 } Snode;
 
 typedef Snode *Stack;
@@ -27,84 +27,84 @@ void display(Stack S);
 
 int main(void) // test funtion
 {
-	Stack newlist = CreateStack(MAX_STACK_SIZE);
-	display(newlist);
-	push(newlist, 3);
-	push(newlist, 10);
-	display(newlist);
-	for (int i = 0; i < 10; i++)
-		push(newlist, i);
-	display(newlist);
-	printf("%d\n", pop(newlist));
-	for (int j = 0; j < 5; j++)
-		printf("%d ", pop(newlist));
-	putchar('\n');
-	display(newlist);
-	return 0;
+    Stack newlist = CreateStack(MAX_STACK_SIZE);
+    display(newlist);
+    push(newlist, 3);
+    push(newlist, 10);
+    display(newlist);
+    for (int i = 0; i < 10; i++)
+        push(newlist, i);
+    display(newlist);
+    printf("%d\n", pop(newlist));
+    for (int j = 0; j < 5; j++)
+        printf("%d ", pop(newlist));
+    putchar('\n');
+    display(newlist);
+    return 0;
 }
 
 Stack CreateStack(int size)
 {
-	Stack newS = (Stack)calloc(size, sizeof(Snode));
-	newS->head = (struct Node *)calloc(size, sizeof(struct Node));
-	newS->head->next = NULL;
-	newS->cursize = 0;
-	newS->maxsize = size;
-	return newS;
+    Stack newS = (Stack)calloc(size, sizeof(Snode));
+    newS->head = (struct Node *)calloc(size, sizeof(struct Node));
+    newS->head->next = NULL;
+    newS->cursize = 0;
+    newS->maxsize = size;
+    return newS;
 }
 
 bool IsFull(Stack S)
 {
-	return S->cursize == S->maxsize;
+    return S->cursize == S->maxsize;
 }
 
 bool IsEmpty(Stack S)
 {
-	return S->cursize == 0;
+    return S->cursize == 0;
 }
 
 void push(Stack S, ElementType item)
 {
-	if (IsFull(S))
-	{
-		fprintf(stderr, "The stack is full.\n");
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
-		temp->data = item;
-		temp->next = S->head;
-		S->head = temp;
-		S->cursize++;
-	}
+    if (IsFull(S))
+    {
+        fprintf(stderr, "The stack is full.\n");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+        temp->data = item;
+        temp->next = S->head;
+        S->head = temp;
+        S->cursize++;
+    }
 }
 
 ElementType pop(Stack S)
 {
-	if (IsEmpty(S))
-	{
-		fprintf(stderr, "The stack is empty.\n");
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		int res = S->head->data;
-		S->head = S->head->next;
-		S->cursize--;
-		return res;
-	}
+    if (IsEmpty(S))
+    {
+        fprintf(stderr, "The stack is empty.\n");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        int res = S->head->data;
+        S->head = S->head->next;
+        S->cursize--;
+        return res;
+    }
 }
 
 void display(Stack S)
 {
-	Stack temp = (Stack)malloc(sizeof(Stack));
-	temp->head = (struct Node *)malloc(sizeof(struct Node));
-	temp->head = S->head;
-	while (temp->head->next != NULL)
-	{
-		printf("%d ", temp->head->data);
-		temp->head = temp->head->next;
-	}
-	putchar('\n');
+    Stack temp = (Stack)malloc(sizeof(Stack));
+    temp->head = (struct Node *)malloc(sizeof(struct Node));
+    temp->head = S->head;
+    while (temp->head->next != NULL)
+    {
+        printf("%d ", temp->head->data);
+        temp->head = temp->head->next;
+    }
+    putchar('\n');
 }

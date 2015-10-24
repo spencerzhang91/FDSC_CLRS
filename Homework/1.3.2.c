ca2e4f4eb1d46b5fa983bc525ac352aa70bb96a7
Spencer Zhang <spencerpomme@live.com>
2015-10-24 22:51:38 +0800

    1024

---
+++ Homework/1.3.2.c
@@ -0,0 +1,19 @@
+/* Excercise 1.3.2: Implementation of a set */
+#include <stdio.h>
+#define ElementType int
+#define MAXNUM 100;
+
+typedef struct {
+	ElementType *data;
+	int elementnum;
+} Set;
+
+ElementType Create(int len);
+void Insert(Set dataset, ElementType ele);
+void Remove(Set dataset);
+bool Is_In(Set dataset, ElementType ele);
+void Uniont(Set database1, Set database2);
+void Inerception(Set database1, Set database1);
+void Difference(Set database1, Set database2);
+
+

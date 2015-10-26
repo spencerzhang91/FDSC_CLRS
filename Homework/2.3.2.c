2708ca7167970dfa6952bedabebda769ab9c7ca2
Spencer Zhang <spencerpomme@live.com>
2015-10-26 16:46:46 +0800

    ...

--- Homework/2.3.2.c
+++ Homework/2.3.2.c
@@ -1,8 +1,41 @@
 /* Excercise 2.3.2.c polynomial multiplication */
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef PolyNode *Polynomial;
+struct PolyNode {
+	int coef;
+	int expo;
+	Polynomial next;
+};
+
+void printpoly(Polynomial P);
+void readpoly(void);
+void mult(Polynomial P1, Polynomial P2);
+
 int main(void)
 {
-	puts("Hello, world")
+	Polynomial P1 = readpoly(void);
+	Polynomial P2 = readpoly(void);
+	Polynomial PM;
+	PM = mult(P1, P2);
+	printpoly(PM);
+
 	return 0;
 }
 
+void printpoly(Polynomial P)
+{
+
+}
+
+void readpoly(void)
+{
+
+}
+
+void mult(Polynomial P1, Polynomial P2)
+{
+
+}

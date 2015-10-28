4b07e4bcca880ff7552e10446b88617d4ba4a7f2
Spencer Zhang <spencerpomme@live.com>
2015-10-28 09:41:02 +0800

    OCT28

--- Notes/4-2_linkelistnode.c
+++ Notes/4-2_linkelistnode.c
@@ -5,8 +5,8 @@
 #define VAL 10;
 
 struct Node {
-	int data;
-	struct Node *next;
+    int data;
+    struct Node *next;
 };
 
 typedef struct Node *Linked;
@@ -14,6 +14,7 @@ Linked create2(void);
 void InsertA(Linked *ptr, Linked node);
 Linked InsertB(Linked ptr, Linked node);
 void display(Linked ptr);
+void delete(Linked *ptr, Linked trail);
 
 int main(void)
 {
@@ -27,53 +28,56 @@ int main(void)
     newtwo = InsertB(TwoNode, TwoNode);
     display(newtwo);
 
+    delete(&newtwo, newtwo->next);
+    display(newtwo);
+
     return 0;    
 }
 
 Linked create2(void)
 {
-	/* create linked list with two nodes */
-	Linked head = (Linked)malloc(sizeof(struct Node));
-	Linked tail = (Linked)malloc(sizeof(struct Node));
-	head->data = 1;
-	head->next = tail;
-	tail->data = 2;
-	tail->next = NULL;
-	return head;
+    /* create linked list with two nodes */
+    Linked head = (Linked)malloc(sizeof(struct Node));
+    Linked tail = (Linked)malloc(sizeof(struct Node));
+    head->data = 1;
+    head->next = tail;
+    tail->data = 2;
+    tail->next = NULL;
+    return head;
 }
 
 void InsertA(Linked *ptr, Linked node)
 {
-	/* insert after node if list is not empty */
-	Linked temp = (Linked)malloc(sizeof(struct Node));
-	temp->data = VAL;
-	if (*ptr)
-	{
-		temp->next = node->next;
-		node->next = temp;
-	}
-	else
-	{
-		temp->next = NULL;
-		*ptr = temp;
-	}
+    /* insert after node if list is not empty */
+    Linked temp = (Linked)malloc(sizeof(struct Node));
+    temp->data = VAL;
+    if (*ptr)
+    {
+        temp->next = node->next;
+        node->next = temp;
+    }
+    else
+    {
+        temp->next = NULL;
+        *ptr = temp;
+    }
 }
 
 Linked InsertB(Linked ptr, Linked node)
 {
-	Linked temp = (Linked)malloc(sizeof(struct Node));
-	temp->data = VAL;
-	if (ptr)
-	{
-		temp->next = node->next;
-		node->next = temp;
-	}
-	else
-	{
-		temp->next = NULL;
-		ptr = temp;
-	}
-	return ptr;
+    Linked temp = (Linked)malloc(sizeof(struct Node));
+    temp->data = VAL;
+    if (ptr)
+    {
+        temp->next = node->next;
+        node->next = temp;
+    }
+    else
+    {
+        temp->next = NULL;
+        ptr = temp;
+    }
+    return ptr;
 }
 
 void display(Linked list)
@@ -86,4 +90,17 @@ void display(Linked list)
     }
         
     printf("\n");
+}
+
+void delete(Linked *ptr, Linked trail)
+{
+    Linked temp;
+    if (trail)
+    {
+        temp = trail->next;
+        trail->next = temp->next;
+    }
+    else
+        (*ptr) = (*ptr)->next;
+    free(temp);
 }

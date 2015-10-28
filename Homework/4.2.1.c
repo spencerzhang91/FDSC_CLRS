c5aa1e68d386621fa4a5de4759081df1e9071381
Spencer Zhang <spencerpomme@live.com>
2015-10-28 09:37:23 +0800

    OCT28

--- Homework/4.2.1.c
+++ Homework/4.2.1.c
@@ -1,12 +1,13 @@
 /* Excercise 4.2.1.c of char 4 on page 92 */
 void delete(Linked *ptr, Linked trail)
 {
-	if (trail)
-	{
-		Linked temp = trail->next;
-		trail->next = temp->next;
-	}
-	else
-		(*ptr) = (*ptr)->next;
-	free(temp);
+    Linked temp;
+    if (trail)
+    {
+        temp = trail->next;
+        trail->next = temp->next;
+    }
+    else
+        (*ptr) = (*ptr)->next;
+    free(temp);
 }

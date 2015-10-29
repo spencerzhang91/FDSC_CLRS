0eb52db82392073ae26202b4a2d1b2ebeb25a828
Spencer Zhang <spencerpomme@live.com>
2015-10-29 21:23:29 +0800

    OCT29

--- Homework/4.2.6.c
+++ Homework/4.2.6.c
@@ -1,29 +1,36 @@
 /* Excercise 4.2.6 */
 Linked MergeLists(Linked list1, Linked list2)
 {
+    /* create a new list while remainning original ones */
     Linke result = (Linked)malloc(sizeof(struct Node));
     while (list1 && list2)
     {
         if (list1->data <= list2->data)
         {
-            Insert(result, list1->data);
+            append(result, list1->data);
             list1 = list1->next;
         }
         else
         {
-            Insert(result, list2->data);
+            append(result, list2->data);
             list2 = list2->next;
         }
     }
     while (list1)
     {
-        Insert(result, list1->data);
+        append(result, list1->data);
         list1 = list1->next;
     }
     while (list2)
     {
-        Insert(result, list2->data);
+        append(result, list2->data);
         list2 = list2->next;
     }
     return result;
+}
+
+void MergeList(Linked list1, Linked list2)
+{
+	/* original lists gone, merged into new one */
+	
 }

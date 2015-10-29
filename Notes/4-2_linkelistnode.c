cdb6e1eaf5b90148ec0dddffa6eafd67bfb0db12
Spencer Zhang <spencerpomme@live.com>
2015-10-29 23:57:08 +0800

    OCT29

--- Notes/4-2_linkelistnode.c
+++ Notes/4-2_linkelistnode.c
@@ -217,23 +217,23 @@ Linked MergeLists(Linked list1, Linked list2)
     {
         if (list1->data <= list2->data)
         {
-            Insert(result, list1->data);
+            append(result, list1->data);
             list1 = list1->next;
         }
         else
         {
-            Insert(result, list2->data);
+            append(result, list2->data);
             list2 = list2->next;
         }
     }
     while (list1)
     {
-        Insert(result, list1->data);
+        append(result, list1->data);
         list1 = list1->next;
     }
     while (list2)
     {
-        Insert(result, list2->data);
+        append(result, list2->data);
         list2 = list2->next;
     }
     return result;

3f5d56e510952244bd18553f06a3d501f906d94c
Spencer Zhang <spencerpomme@live.com>
2015-10-30 13:52:59 +0800

    OCT30

--- Homework/4.2.7.c
+++ Homework/4.2.7.c
@@ -2,16 +2,25 @@
 Linked weave(Linked list1, Linked list2)
 {
     /* weave two linked lists */
-    Linked newlist = (Linked)malloc(sizeof(struct Node));
-    newlist->next = list1;
-    Linked curr = list1;
+    Linked newlist = list1;
+    Linked curr = newlist;
+    int flag = 1;
     while (list1 && list2)
     {
-        list1 = list1->next;
-        curr->next = list2;
-        list2 = list2->next;
-        curr->next = list1;
-        curr = curr->next;
+        if (flag == 1)
+        {
+            curr->next = list1;
+            list1 = list1->next;
+            curr = curr->next;
+            flag = 0;
+        }
+        else
+        {
+            curr->next = list2;
+            list2 = list2->next;
+            curr = curr->next;
+            flag = 1;
+        }
     }
     if (list1)
         curr->next = list1;

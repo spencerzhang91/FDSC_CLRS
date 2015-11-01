eeeb3ebb0d561ecaa9292d7ba234243f245705e5
Spencer Zhang <spencerpomme@live.com>
2015-11-01 23:59:47 +0800

    NOV01

--- Notes/4-13_CircularList.c
+++ Notes/4-13_CircularList.c
@@ -1,2 +1,22 @@
 /* 4-13, 4-14, 4-15 */
 /* On page 100 - 102 of book FDSC */
+
+Linked get_node(void)
+{
+	/* provide a node from the available list */
+	Linked node;
+	if (avail)
+	{
+		node = avail;
+		avail = avail->next;
+	}
+	else
+		node = (Linked)malloc(sizeof(struct Node));
+	return node;
+}
+
+void ret_node(Linked ptr)
+{
+	/* return a node to the available list */
+
+}

c65d901fb3408c40038295e5cf7ff090ffec654f
Spencer Zhang <spencerpomme@live.com>
2015-11-02 12:57:32 +0800

    NOV02
    time flies

---
+++ Notes/4-17_reverseLinkedList.c
@@ -0,0 +1,35 @@
+/* 4-17 invert linked list */
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node *Linked;
+struct Node {
+    int val;
+    Linked next;
+};
+
+void invert(Linked list);
+
+int main(void)
+{
+    Linked temp = (Linked)malloc(sizeof(struct Node));
+    temp->val = 3;
+    temp->next = NULL;
+    
+    printf("%d\n", temp->val);
+    return 0;
+}
+
+void invert(linked list)
+{
+	Linked prev, curr;
+	curr = NULL;
+	while (list)
+	{
+		prev = curr;
+		curr = list;
+		list = list->next;
+		curr->next = prev;
+	}
+	return curr;
+}

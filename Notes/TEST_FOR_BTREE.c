83280efbf72ba8696e6055c502a6654c1c339624
Spencer Zhang <spencerpomme@live.com>
2015-11-10 15:04:30 +0800

    NOV10

--- Notes/TEST_FOR_BTREE.c
+++ Notes/TEST_FOR_BTREE.c
@@ -1,21 +1,97 @@
-/* Preorder, inorder and postorder traversal algorithm of binary tree */
-/* Using stack ADT to implement iteratively */
-/* This file need to be compared with file '5-4_iterBTreeTraversal.c'
-since this two is identical in nature and comparable in implementation
-I personaly reckon this version as better implemented */
-#define MAXSIZE 10                     // the maxsize of tree nodes
+/* Test file for 5-4 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define ElementType int
+#define MAX_STACK_SIZE 100
+#define MAXSIZE 10
 
+/* binary tree ADT struct definition */
 typedef struct treeNode *BTree;
 struct treeNode {
-    int data;
+    ElementType data;
     BTree left;
     BTree right;
 };
 
+/* stack ADT struct definition */
+typedef struct {
+    ElementType *array;
+    int top;
+    int maxsize;
+} Snode;
+typedef Snode *Stack;
+
+/* stack ADT methods */
+Stack CreateStack(int size);
+bool IsFull(Stack S);
+bool IsEmpty(Stack S);
+void push(Stack S, ElementType item);
+ElementType pop(Stack S);
+void display(Stack S);
+
+/* binary tree traversal */
 void iter_preorder(BTree root);
 void iter_inorder(BTree root);
 void iter_postorder(BTree root);
+BTree CreateBTree(char *preorder, char *inorder); // yet done
 
+int main(void)
+{
+	
+	return 0;
+}
+
+/* stack ADT methods */
+Stack CreateStack(int size)
+{
+    Stack newS = (Stack)malloc(sizeof(Stack));
+    newS->array = (ElementType *)calloc(size, sizeof(ElementType));
+    newS->top = -1;
+    newS->maxsize = size;
+    return newS;
+}
+
+bool IsFull(Stack S)
+{
+    return S->top - 1 == S->maxsize;
+}
+
+bool IsEmpty(Stack S)
+{
+    return S->top == -1;
+}
+
+void push(Stack S, ElementType item)
+{
+    if (IsFull(S))
+    {
+        fprintf(stderr, "The stack is full.\n");
+        exit(EXIT_FAILURE);
+    }
+    else
+        S->array[++(S->top)] = item;
+}
+
+ElementType pop(Stack S)
+{
+    if (IsEmpty(S))
+    {
+        fprintf(stderr, "The stack is empty.\n");
+        exit(EXIT_FAILURE);
+    }
+    else
+        return S->array[(S->top)--];
+}
+
+void display(Stack S)
+{
+    for (int i = 0; i < S->top + 1; i++)
+        printf("%d ", S->array[i]);
+    putchar('\n');
+}
+
+/* binary tree traversal methods */
 void iter_preorder(BTree root)
 {
     S = CreateStack(MAXSIZE);          // create and initialize stack
@@ -83,4 +159,11 @@ void iter_postorder(BTree root)
         if (!isEmpty(S))
             tree = pop(S);
     }
+}
+
+BTree CreateBTree(char *preorder, char *inorder)
+{
+	/* This function use preorder and inorder traversal sequence of a tree to
+	create a unique binary tree. The two input is string type and return the 
+	head of the constructed tree. */
 }

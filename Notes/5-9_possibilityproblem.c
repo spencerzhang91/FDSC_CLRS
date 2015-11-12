a10435e2ae01beb7f5533e7f7bba69319ef7da02
Spencer Zhang <spencerpomme@live.com>
2015-11-12 17:35:09 +0800

    NOV12

--- Notes/5-9_possibilityproblem.c
+++ Notes/5-9_possibilityproblem.c
@@ -2,7 +2,33 @@
 typedef enum {not, and, or, true, false} logical;
 typedef struct treeNode *BTree;
 struct treeNode {
-    int data;
+    int value;
+    logical data;
     BTree left;
     BTree right;
 };
+
+void postorder_eval(BTree node)
+{
+	/* post order traversal to evaluate a propositional calculus tree. */
+	if (node)
+	{
+		postorder_eval(node->left);
+		postorder_eval(node->right);
+		switch (node->data)
+		{
+			case not: node->value = !node->right->value;
+			break;
+			case and: node->value = node->left->value && node->right->value;
+			break;
+			case or: node->value = node->left->value || node->right->value;
+			break;
+			case true: node->value = 1;
+			break;
+			case false: node->value = 0;
+			break;
+			default: printf("Something fucking goes wrong!\n");
+		}
+
+	}
+}

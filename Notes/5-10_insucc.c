7a0c63917ac973a7c8d4838eff5a4c614b9a5de4
Spencer Zhang <spencerpomme@live.com>
2015-11-14 21:53:55 +0800

    NOV14

--- Notes/5-10_insucc.c
+++ Notes/5-10_insucc.c
@@ -1,5 +1,5 @@
 /* learning binary tree with thread from FDSC on page 133-137 */
-/* code list 5-10 */
+/* code list 5-10 and 5-11 */
 typedef struct threaded_tree *thread_pointer;
 struct threaded_tree {
 	bool left_thread;
@@ -18,4 +18,16 @@ threaded_pointer insucc(threaded_pointer node)
 		while (!temp->left_child)
 			temp = temp->left_child;
 	return temp;
+}
+
+void tinorder(threaded_pointer tree)
+{
+	/* traversal the threaded binary tree inorder */
+	threaded_pointer temp = tree;
+	for (;;)
+	{
+		temp = insucc(temp);
+		if (temp == tree) break;
+		printf("%d ", temp->data);
+	}
 }

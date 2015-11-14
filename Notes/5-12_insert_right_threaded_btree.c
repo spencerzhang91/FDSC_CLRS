8830932112dc02d86014364b5a324ac86e535b30
Spencer Zhang <spencerpomme@live.com>
2015-11-14 23:04:11 +0800

    NOV14

--- Notes/5-12_insert_right_threaded_btree.c
+++ Notes/5-12_insert_right_threaded_btree.c
@@ -1,2 +1,18 @@
 /* learning binary tree with thread from FDSC on page 133-137 */
 /* code list 5-12 */
+void insert_right(threaded_pointer parent, threaded_pointer child)
+{
+	/* insert child as the right child of parent in a threaded binary tree */
+	threaded_pointer temp;
+	child->right_child = parent->right_child;
+	child->right_thread = parent->right_thread;
+	child->left_child = parent;
+	child->left_thread = true;
+	parent->right_child = child;
+	parent->right_thread = false;
+	if (!child->right_thread)
+	{
+		temp = insucc(child);
+		temp->left_child = child;
+	}
+}

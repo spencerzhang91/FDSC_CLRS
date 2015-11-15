6603a745cfca5e9bc938f01292ee27bfb66ec1fe
Spencer Zhang <spencerpomme@live.com>
2015-11-15 16:40:21 +0800

    NOV15

--- Notes/5_BTreeGenerator.c
+++ Notes/5_BTreeGenerator.c
@@ -7,8 +7,9 @@ order traversal, missing node is replaced by '#': (LeetCode way)
           /\    \                \
          D  E    F                D
           (tree1)                (tree2)
-tree1: {'A','B','C','D','E','#','F'} or "ABCDE#F";
-tree2: {'A','B','#','#','D'} or "AB##D";
+tree1: {'0','A','B','C','D','E','#','F'} or "0ABCDE#F";
+tree2: {'0','A','B','#','#','D'} or "0AB##D";
+First '0' is the sentinel node for subfix conciseness
 The function takes in an array or a tring and returns the root of the tree
 */
 #define ElementType char // also can be changed to int, float, etc.
@@ -19,4 +20,38 @@ struct treeNode {
 	BTree right;
 };
 
+BTree createBTree(char *array, int len)
+{
+	/* generate a binary tree */
+	int i;
+	for (i = 1; i * 2 + 1 < len; i++)
+	{
+		BTree parent = makenode(array[i]);
+		BTree leftchild = makenode(array[i*2]);
+		BTree rightchild = makenode(array[i*2+1]);
+		if (leftchild)
+			parent->left = leftchild;
+		if (rightchild)
+			parent->right = rightchild;
+	}
+
+}
+
+BTree makenode(ElementType data)
+{
+	/* create a tree node and return its pointer if data is not '#' */
+	if (data != '#')
+	{
+		BTree node = (BTree)malloc(sizeof(struct treeNode));
+		if (!node)
+		{
+			fprintf(stderr, "Memory full.\n");
+			exit(EXIT_FAILURE);
+		}
+		node->data = data; node->left = NULL; node->right = NULL;
+		return node;
+	}
+	else
+		return NULL;
+}
 

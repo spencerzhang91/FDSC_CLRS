5f71e464cc2ac4510910455e326a3dfc411be1c7
Spencer Zhang <spencerpomme@live.com>
2015-11-18 15:45:26 +0800

    NOV18

---
+++ Notes/5-13_maxheap_insert.c
@@ -0,0 +1,29 @@
+/* insertion of max heap */
+#define MAX_ELEMENTS 200
+#define HEAP_FULL(n) (n == MAX_ELEMENTS - 1)
+#define HEAP_EMPTY(n) (!n)
+typedef struct {
+    int key;
+    /* other fields */
+} element;
+
+element heap[MAX_ELEMENTS];
+int n = 0;
+
+void insert_max_heap(element item, int *n)
+{
+    /* insert item into a max heap of current size *n */
+    int i;
+    if (HEAP_FULL(*n))
+    {
+        fprintf(stderr, "The heap is full.\n");
+        exit(EXIT_FAILURE);
+    }
+    i = ++(*n);
+    while ((i != 1) && (item.key > heap[i/2].key))
+    {
+        heap[i] = heap[i/2];
+        i /= 2;
+    }
+    heap[i] = item;
+}

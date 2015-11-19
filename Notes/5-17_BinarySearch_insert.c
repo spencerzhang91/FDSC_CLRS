d2f67b0c3eecc95f4a0c62d96a88267b3c642998
Spencer Zhang <spencerpomme@live.com>
2015-11-19 16:53:26 +0800

    NOV19

--- Notes/5-17_BinarySearch_insert.c
+++ Notes/5-17_BinarySearch_insert.c
@@ -1,1 +1,46 @@
 /* code fragment 5-17: insert a new item into the binary search tree */
+
+typedef struct treeNode *BiTree;
+struct treeNode {
+    int data;
+    BiTree left;
+    BiTree right;
+};
+
+void insert_node(BiTree *pointer, int num)
+{
+    BiTree
+}
+
+BiTree search_iter(BiTree root, int key)
+{
+    /* return a pointer to the node that contains key. If there's no such key,
+    return NULL. */
+    while (root)
+    {
+        if (root->data == key) return root;
+        if (key < root->data) root = root->left;
+        else root = root->right;
+    }
+    return NULL;
+}
+
+BiTree modified_search(BiTree root, int key)
+{
+    if (!root)
+    {
+        printf("The tree is empty.\n");
+        return NULL;
+    }
+    while (root->left || root->right)
+    {
+        if (root->data == key)
+        {
+            printf("The key %d is already in the tree.\n", key);
+            return NULL;
+        }
+        if (key < root->data) root = root->left;
+        else root = root->right;
+    }
+    return root;
+}

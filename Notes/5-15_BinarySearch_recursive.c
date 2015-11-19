bd3f2e09719ea4a2039f281211672d351be03ee0
Spencer Zhang <spencerpomme@live.com>
2015-11-19 15:56:23 +0800

    NOV19

---
+++ Notes/5-15_BinarySearch_recursive.c
@@ -0,0 +1,23 @@
+/* code fragment 5-15: recursive way to do binary search 
+   The time complexity is O(h) and additional stack space consumption O(h) 
+   h is the height of the tree.
+*/
+
+typedef struct treeNode *BiTree;
+struct treeNode {
+    int data;
+    BiTree left;
+    BiTree right;
+};
+
+BiTree search_recr(BiTree root, int key)
+{
+    /* return a pointer to the node that contains key. If there's no such key,
+    return NULL. */
+    if (!root) return NULL;
+    if (key == root->data) return root;
+    if (key < root->data)
+        return search_recr(root->left, key);
+    else
+        return search_recr(root->right, key);
+}

05dc7e4c7cd7b6561875039dbc35dbba7c4e4df8
Spencer Zhang <spencerpomme@live.com>
2015-11-19 23:58:47 +0800

    NOV19

--- Notes/5_Arraybased_MinHeapADT.c
+++ Notes/5_Arraybased_MinHeapADT.c
@@ -1,1 +1,23 @@
 /* Implementation of array based max heap ADT */
+#include <stdlib.h>
+#define MAXSIZE 100
+#define MINDATA 10000
+
+typedef struct HeapStruct *MinHeap;
+struct HeapStruct {
+    ElementType *elements;      // array
+    int size;                   // current size
+    int capacity;               // max element number
+}
+
+MinHeap Create(int MAXSIZE)
+{
+    /* create a emtpy max heap with capacity of MAXSIZE */
+    MaxHeap H = (MaxHeap)malloc( sizeof(struct HeapStruct));
+    H->elements = (ElementType)malloc((MAXSIZE+1) * sizeof(ElementType));
+    H->size = 0;
+    H->capacity = MAXSIZE;
+    H->elements[0] = MINDATA;   // sentinel
+
+    return H;
+}

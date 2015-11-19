be2ee050fce68d1b4373f5f40147615db6793e37
Spencer Zhang <spencerpomme@live.com>
2015-11-19 17:41:38 +0800

    NOV19

--- Notes/5-17_BinarySearch_insert.c
+++ Notes/5-17_BinarySearch_insert.c
@@ -9,7 +9,26 @@ struct treeNode {
 
 void insert_node(BiTree *pointer, int num)
 {
-    BiTree
+    
+    BiTree ptr; // to point to num container node(to be created on the fly)
+    BiTree temp = modified_search(*pointer, num);
+    if (temp || !*pointer) // means num is not in the tree
+    {
+        ptr = (BiTree)malloc(sizeof(treeNode));
+        if (!ptr)
+        {
+            fprintf(stderr, "The memory is full.\n");
+            exit(EXIT_FAILURE);
+        }
+        ptr->data = num; ptr->left = ptr->right = NULL;
+        if (*pointer) // which means the tree is not empty
+        {
+            if (num < temp->data) temp->left = ptr;
+            else temp->right = ptr;
+        }
+        else // pointer is an empty tree
+            *pointer = ptr;            
+    }
 }
 
 BiTree search_iter(BiTree root, int key)

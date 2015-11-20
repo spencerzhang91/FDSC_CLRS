f457ebde8cc01094ef5e3b1a1278f45f4058a851
Spencer Zhang <spencerpomme@live.com>
2015-11-20 23:46:12 +0800

    NOV20

--- Notes/5-17_BinarySearch_delete.c
+++ Notes/5-17_BinarySearch_delete.c
@@ -1,4 +1,6 @@
 /* code fragment 5-17: delete a new item from the binary search tree */
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct treeNode *BiTree;
 struct treeNode {
@@ -7,11 +9,16 @@ struct treeNode {
     BiTree right;
 };
 
+BiTree createSearchTree(int init);
+BiTree delete_recr(BiTree root, int number);
+void delete_iter(BiTree *root, int number);
+BiTree findmax(BiTree node);
+
 int main(void)
 {
     BiTree tree = createSearchTree(3);
-    delete_iter(tree, 2);
-    delete_iter(tree, 3);
+    delete_recr(tree, 2);
+    delete_recr(tree, 3);
     
     return 0;
 }
@@ -29,29 +36,29 @@ BiTree delete_recr(BiTree root, int number)
     BiTree temp;
     if (!root) printf("Not found.\n");
     else if (number < root->data)
-        root->left = delete(root->left, number);
+        root->left = delete_recr(root->left, number);
     else if (number > root->data)
-        root->right = delete(root->right, number);
+        root->right = delete_recr(root->right, number);
     else
     {
         if (root->left && root->right)
         {
             temp = findmax(root->left); // or findmin(root->right);
             root->data = temp->data;
-            root->left = delete(root->left, root->data);
+            root->left = delete_recr(root->left, root->data);
         }
         else
         {
             temp = root;
-            if (!right) root = root->left;
-            else if (!left) root = root->right;
+            if (!root->right) root = root->left;
+            else if (!root->left) root = root->right;
             free(temp);
         }
         return root; // to return is for recurse
     }
 }
 
-void delete_iter(BiTree *root, int number)
+void delete_iter(BiTree *root, int number) // something wrong here!
 {
     BiTree temp;
     if (!root) printf("Empty tree!\n");
@@ -68,13 +75,13 @@ void delete_iter(BiTree *root, int number)
                 temp = findmax((*root)->left);
                 if (temp->left)
                     (*root)->left->right = temp->left;
-                root->data = temp->data;
+                (*root)->data = temp->data;
             }
             else
             {
                 temp = (*root);
-                if (!right) (*root) = (*root)->left;
-                else if (!left) (*root) = (*root)->right;
+                if (!(*root)->right) (*root) = (*root)->left;
+                else if (!(*root)->left) (*root) = (*root)->right;
             }
             free(temp);
         }

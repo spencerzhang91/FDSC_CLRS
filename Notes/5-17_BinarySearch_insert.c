88854a6d66d99f726de3309b0d875424e9999f2e
Spencer Zhang <spencerpomme@live.com>
2015-11-20 19:38:12 +0800

    NOV20

--- Notes/5-17_BinarySearch_insert.c
+++ Notes/5-17_BinarySearch_insert.c
@@ -31,18 +31,6 @@ void insert_node(BiTree *pointer, int num)
     }
 }
 
-BiTree search_iter(BiTree root, int key)
-{
-    /* return a pointer to the node that contains key. If there's no such key,
-    return NULL. */
-    while (root)
-    {
-        if (root->data == key) return root;
-        if (key < root->data) root = root->left;
-        else root = root->right;
-    }
-    return NULL;
-}
 
 BiTree modified_search(BiTree root, int key)
 {

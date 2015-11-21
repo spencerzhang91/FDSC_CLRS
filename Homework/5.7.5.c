5eb9503299c8a889a8ee7e07d0ccd6643b41d8d9
Spencer Zhang <spencerpomme@live.com>
2015-11-21 20:03:26 +0800

    NOV21

--- Homework/5.7.5.c
+++ Homework/5.7.5.c
@@ -1,30 +1,32 @@
 /* excercise 5.7.5.c */
-void delete_iter(BiTree *root, int number) // something wrong here!
+void delete_iter(BiTree root, int number) // something wrong here!
 {
     BiTree temp;
     if (!root) printf("Empty tree!\n");
     while (root)
     {
-        if (number < (*root)->data)
-            (*root) = (*root)->left;
-        else if (number > (*root)->data)
-            (*root) = (*root)->right;
+        if (number < root->data)
+            root = root->left;
+        else if (number > root->data)
+            root = root->right;
         else
         {
-            if ((*root)->left && (*root)->right)
+            if (root->left && root->right)
             {
-                temp = findmax((*root)->left);
+                temp = findmax(root->left);
                 if (temp->left)
-                    (*root)->left->right = temp->left;
-                (*root)->data = temp->data;
+                    root->left->right = temp->left;
+                root->data = temp->data;
             }
             else
             {
-                temp = (*root);
-                if (!(*root)->right) (*root) = (*root)->left;
-                else if (!(*root)->left) (*root) = (*root)->right;
+                temp = root;
+                if (!root->right) root = root->left;
+                else if (!root->left) root = root->right;
             }
+            temp = NULL;
             free(temp);
+            printf("Successfully deleted number %d from tree.\n", number);
         }
     }
 }

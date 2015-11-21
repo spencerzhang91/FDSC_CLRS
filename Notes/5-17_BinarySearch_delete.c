a5857954bd2d18917b2a46efdc4cce2d233f5c84
Spencer Zhang <spencerpomme@live.com>
2015-11-21 19:12:59 +0800

    NOV21

--- Notes/5-17_BinarySearch_delete.c
+++ Notes/5-17_BinarySearch_delete.c
@@ -9,16 +9,19 @@ struct treeNode {
     BiTree right;
 };
 
-BiTree createSearchTree(int init);
-BiTree delete_recr(BiTree root, int number);
-void delete_iter(BiTree *root, int number);
+BiTree createSearchTree(int init); //
+BiTree delete_recr(BiTree root, int number);//
+
+void delete_iter(BiTree root, int number);
 BiTree findmax(BiTree node);
+void preorder(BiTree root);
+void insert_node(BiTree *pointer, int num);
+BiTree modified_search(BiTree root, int key);
 
 int main(void)
 {
     BiTree tree = createSearchTree(3);
-    delete_recr(tree, 2);
-    delete_recr(tree, 3);
+    preorder(tree);
     
     return 0;
 }
@@ -58,34 +61,40 @@ BiTree delete_recr(BiTree root, int number)
     }
 }
 
-void delete_iter(BiTree *root, int number) // something wrong here!
+
+void delete_iter(BiTree root, int number) // something wrong here!
 {
     BiTree temp;
     if (!root) printf("Empty tree!\n");
     while (root)
     {
-        if (number < (*root)->data)
-            (*root) = (*root)->left;
-        else if (number > (*root)->data)
-            (*root) = (*root)->right;
+        if (number < root->data)
+            root = root->left;
+        else if (number > root->data)
+            root = root->right;
         else
         {
-            if ((*root)->left && (*root)->right)
+            if (root->left && root->right)
             {
-                temp = findmax((*root)->left);
+                temp = findmax(root->left);
                 if (temp->left)
-                    (*root)->left->right = temp->left;
-                (*root)->data = temp->data;
+                    root->left->right = temp->left;
+                root->data = temp->data;
             }
             else
             {
-                temp = (*root);
-                if (!(*root)->right) (*root) = (*root)->left;
-                else if (!(*root)->left) (*root) = (*root)->right;
+                temp = root;
+                if (!root->right) root = root->left;
+                else if (!root->left) root = root->right;
             }
             free(temp);
+            printf("Successfully deleted number %d from tree.\n", number);
+            return;
         }
     }
+    printf("Number %d not found in the tree.\n", number);
+    puts("\n");
+    return;
 }
 
 BiTree findmax(BiTree node)
@@ -93,4 +102,59 @@ BiTree findmax(BiTree node)
     while (node->right)
         node = node->right;
     return node;
+}
+
+void preorder(BiTree root)
+{
+    if (root)
+    {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void insert_node(BiTree *pointer, int num)
+{
+    
+    BiTree ptr; // to point to num container node(to be created on the fly)
+    BiTree temp = modified_search(*pointer, num);
+    if (temp || !*pointer) // means num is not in the tree
+    {
+        ptr = (BiTree)malloc(sizeof(struct treeNode));
+        if (!ptr)
+        {
+            fprintf(stderr, "The memory is full.\n");
+            exit(EXIT_FAILURE);
+        }
+        ptr->data = num; ptr->left = ptr->right = NULL;
+        if (*pointer) // which means the tree is not empty
+        {
+            if (num < temp->data) temp->left = ptr;
+            else temp->right = ptr;
+        }
+        else // pointer is an empty tree
+            *pointer = ptr;            
+    }
+}
+
+
+BiTree modified_search(BiTree root, int key)
+{
+    if (!root)
+    {
+        printf("The tree is empty.\n");
+        return NULL;
+    }
+    while (root->left || root->right)
+    {
+        if (root->data == key)
+        {
+            printf("The key %d is already in the tree.\n", key);
+            return NULL;
+        }
+        if (key < root->data) root = root->left;
+        else root = root->right;
+    }
+    return root;
 }

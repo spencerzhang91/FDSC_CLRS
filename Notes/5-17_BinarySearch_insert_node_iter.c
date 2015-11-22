815d476e31447854370c2b0231e11971dde1b369
Spencer Zhang <spencerpomme@live.com>
2015-11-22 18:53:14 +0800

    NOV22

--- Notes/5-17_BinarySearch_insert_node_iter.c
+++ Notes/5-17_BinarySearch_insert_node_iter.c
@@ -9,65 +9,21 @@ struct treeNode {
     BiTree left;
     BiTree right;
 };
-void insert_node(BiTree *pointer, int num);
-BiTree modified_search(BiTree root, int key);
-BiTree modified_search(BiTree root, int key);
+
+void insert_node(BiTree root, int num);
 void preorder(BiTree tree);
+BiTree search_iter(BiTree root, int key)
 
 int main(void)
 {
     BiTree root = (BiTree)malloc(sizeof(struct treeNode));
-    root->data = 10; root->left = root->right = NULL;
-
-    insert_node(&root, 20);
-    preorder(root);
-
+    root->data = 100; root->left = root->right = NULL;
     return 0;
 }
 
-void insert_node(BiTree *pointer, int num)
+void insert_node(BiTree root, int num)
 {
     
-    BiTree ptr; // to point to num container node(to be created on the fly)
-    BiTree temp = modified_search(*pointer, num);
-    if (temp || !*pointer) // means num is not in the tree
-    {
-        ptr = (BiTree)malloc(sizeof(struct treeNode));
-        if (!ptr)
-        {
-            fprintf(stderr, "The memory is full.\n");
-            exit(EXIT_FAILURE);
-        }
-        ptr->data = num; ptr->left = ptr->right = NULL;
-        if (*pointer) // which means the tree is not empty
-        {
-            if (num < temp->data) temp->left = ptr;
-            else temp->right = ptr;
-        }
-        else // pointer is an empty tree
-            *pointer = ptr;            
-    }
-}
-
-
-BiTree modified_search(BiTree root, int key)
-{
-    if (!root)
-    {
-        printf("The tree is empty.\n");
-        return NULL;
-    }
-    while (root->left || root->right)
-    {
-        if (root->data == key)
-        {
-            printf("The key %d is already in the tree.\n", key);
-            return NULL;
-        }
-        if (key < root->data) root = root->left;
-        else root = root->right;
-    }
-    return root;
 }
 
 void preorder(BiTree tree)
@@ -78,4 +34,17 @@ void preorder(BiTree tree)
         preorder(tree->left);
         preorder(tree->right);
     }
+}
+
+BiTree search_iter(BiTree root, int key)
+{
+    /* return a pointer to the node that contains key. If there's no such key,
+    return NULL. */
+    while (root)
+    {
+        if (root->data == key) return root;
+        if (key < root->data) root = root->left;
+        else root = root->right;
+    }
+    return NULL;
 }

2fee5295d128c93d129c32db95f72f0b312b10cd
Spencer Zhang <spencerpomme@live.com>
2015-11-22 14:22:47 +0800

    NOV22
    debugging...

--- Notes/5-17_BinarySearch_insert_node_iter.c
+++ Notes/5-17_BinarySearch_insert_node_iter.c
@@ -1,4 +1,7 @@
 /* code fragment 5-17: insert a new item into the binary search tree */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct treeNode *BiTree;
 struct treeNode {
@@ -6,6 +9,21 @@ struct treeNode {
     BiTree left;
     BiTree right;
 };
+void insert_node(BiTree *pointer, int num);
+BiTree modified_search(BiTree root, int key);
+BiTree modified_search(BiTree root, int key);
+void preorder(BiTree tree);
+
+int main(void)
+{
+    BiTree root = (BiTree)malloc(sizeof(struct treeNode));
+    root->data = 10; root->left = root->right = NULL;
+
+    insert_node(&root, 20);
+    preorder(root);
+
+    return 0;
+}
 
 void insert_node(BiTree *pointer, int num)
 {
@@ -50,4 +68,14 @@ BiTree modified_search(BiTree root, int key)
         else root = root->right;
     }
     return root;
+}
+
+void preorder(BiTree tree)
+{
+    if (tree)
+    {
+        printf("%d ", tree->data);
+        preorder(tree->left);
+        preorder(tree->right);
+    }
 }

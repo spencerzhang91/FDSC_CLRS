546d3729b2f072b83206dad55bce4c9ac68f6d3b
Spencer Zhang <spencerpomme@live.com>
2015-11-24 00:23:11 +0800

    NOV24
    bug fixed

--- Notes/5-17_BinarySearch_delete_node_iter.c
+++ Notes/5-17_BinarySearch_delete_node_iter.c
@@ -10,11 +10,17 @@ struct treeNode {
     BiTree right;
 };
 
+typedef struct pair *Pairs; // to contain result of modified findmax function
+struct pair {
+    BiTree parent;
+    BiTree child;
+};
+
 void insert_node_iter(BiTree *root, int num);
 void delete_iter(BiTree root, int number);
 void preorder(BiTree root);
 BiTree search_iter(BiTree root, int key);
-BiTree findmax(BiTree node);
+Pairs findmax(BiTree node);
 
 int main(void)
 {
@@ -41,36 +47,42 @@ void delete_iter(BiTree root, int number) // something wrong here!
     /* Find the node in the tree if node->data == number, and find proper
     replacement node in node's sub tree, let node->data = replace->data, and
     then acutally delete the replacement node. */
-    BiTree delptr, dummy, temp;
+    BiTree delptr, temp;
+    BiTree parent, child;
+    Pairs found;
     if ((delptr = search_iter(root, number)) != NULL && root)
     {
         if (delptr->left && delptr->right)
         {
-            dummy = findmax(delptr->left);
-            printf("dummy: %d\n", dummy->data);
-            delptr->data = dummy->data;
-            
+            found = findmax(delptr->left);
+            parent = found->parent; child = found->child;
+            delptr->data = child->data;
+            if (parent) parent->right = child->left;
+            free(child); child = NULL;            
         }
         else
         {
-            dummy = delptr;
+            child = delptr;
             if (!delptr->right) delptr = delptr->left;
             else if (!delptr->left) delptr = delptr->right;
-            dummy->data = delptr->data;
-            dummy->left = delptr->left;
-            dummy->right = delptr->right;
+            free(child); child = NULL;
         }
     }
     else printf("\nThe deletion operation failed because of empty tree"
     "or number not found in the tree.\n");
 }
 
-BiTree findmax(BiTree node)
+Pairs findmax(BiTree node)
 {
     /* find the right most tree node in sub tree of input 'node' */
-    while (node->right)
-        node = node->right;
-    return node;
+    Pairs P = (Pairs)malloc(sizeof(struct pair));
+    P->parent = NULL; P->child = node;
+    while (P->child->right)
+    {
+        P->parent = P->child;
+        P->child = P->child->right;
+    }
+    return P;
 }
 
 void preorder(BiTree root)

e9d4276859dc7187329d0b47f7c4402572fcb46c
Spencer Zhang <spencerpomme@live.com>
2015-11-25 16:34:25 +0800

    NOV25

---
+++ Notes/6-1_dfs_recursive.c
@@ -0,0 +1,13 @@
+/* The recursive implementation of depth first search of graph */
+#include <stdbool.h>
+#define MAX_VERTICES 10
+bool visited[MAX_VERTICES];
+
+void dfs(int v)
+{
+    /* depth first search of a graph beginning with vertex v */
+    node_pointer w;
+    visited[v] = true;
+    printf("%d ", v);
+    for (w = graph)
+}

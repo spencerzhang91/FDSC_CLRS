6425d14f6b3fc8576ef4d570fd270f93f560a57f
Spencer Zhang <spencerpomme@live.com>
2015-11-29 15:23:27 +0800

    NOV29

--- Notes/5-20_findparent.c
+++ Notes/5-20_findparent.c
@@ -1,6 +1,7 @@
 /* code list 5-20: find parent of the set */
 #define MAX_ELEMENTS 100
 int parent[MAX_ELEMENTS] = {-1,4,-1,2,-1,2,0,0,0,4};
+
 int find(int i)
 {
     /* find the root of the tree containing element i. Use the collapsing rule

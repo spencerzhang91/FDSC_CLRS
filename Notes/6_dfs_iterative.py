1c459b2e3b59535ba761dae9426c7c512c63fe58
Spencer Zhang <spencerpomme@live.com>
2015-11-30 23:47:31 +0800

    NOV30

---
+++ Notes/6_dfs_iterative.py
@@ -0,0 +1,19 @@
+# The recursive implementation of depth first search of graph
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class Graph:
+    def __init__(self, vertices: list):
+        self.vertices = vertices
+        self.vnum = len(vertices)
+
+    def connect(self, edges: list):
+        pass
+
+
+
+
+
+

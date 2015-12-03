fcfd96dfb0577bb0475dae0218673a9dfeb4a4ba
Spencer Zhang <spencerpomme@live.com>
2015-12-03 22:53:59 +0800

    DEC03

--- Notes/6-1_dfs_iterative.c
+++ Notes/6-1_dfs_iterative.c
@@ -1,1 +1,40 @@
 /* The iterative implementation of depth first search of graph */
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define MaxVertexNum 100
+typedef int Vertex;                         // subfix to represent vertex
+typedef int Weight;                         // use int to represent weight
+typedef char DataType;                      // data type of vertex data
+
+/* definition of edge */
+typedef struct edge *Edge;
+struct edge {
+    Vertex vr;                              // row subfix
+    Vertex vc;                              // col subfix
+    Weight wt;                               // weight of the edge
+};
+
+/* definition of adjacency node */
+typedef struct node *nodeptr;
+struct node {
+    Vertex adjv;
+    Weight wt;
+    DataType data;
+    nodeptr next;
+};
+
+/* definition of linked lists based graph */
+typedef struct Gnode *LGraph;
+struct Gnode {
+    int vertex_num;
+    int edge_num;
+    nodeptr G[MaxVertexNum];                    // adjacency linked list
+};
+
+void dfs_iterative(LGraph graph, Vertex start)
+{
+    nodeptr curr = graph->G[start];
+    bool visited[graph->vertex_num];
+}
--- Notes/6-1_dfs_recursive_.c
+++ Notes/6-1_dfs_recursive_.c
@@ -1,11 +1,41 @@
 /* The recursive implementation of depth first search of graph */
-#include <stdbool.h>
-#define MAX_VERTICES 10
+#include <stdio.h>
+#include <stdlib.h>
+#define MaxVertexNum 100
 bool visited[MAX_VERTICES];
 
-void dfs(int v)
+typedef int Vertex;                         // subfix to represent vertex
+typedef int Weight;                         // use int to represent weight
+typedef char DataType;                      // data type of vertex data
+
+/* definition of edge */
+typedef struct edge *Edge;
+struct edge {
+    Vertex vr;                              // row subfix
+    Vertex vc;                              // col subfix
+    Weight wt;                               // weight of the edge
+};
+
+/* definition of adjacency node */
+typedef struct node *nodeptr;
+struct node {
+    Vertex adjv;
+    Weight wt;
+    DataType data;
+    nodeptr next;
+};
+
+/* definition of linked lists based graph */
+typedef struct Gnode *LGraph;
+struct Gnode {
+    int vertex_num;
+    int edge_num;
+    nodeptr G[MaxVertexNum];                    // adjacency linked list
+};
+
+void dfs_recursive(int v)
 {
-    /* depth first search of a graph beginning with vertex v */
+    /* depth first search of a graph beginning with vth vertex */
     node_pointer w;
     visited[v] = true;
     printf("%d ", v);

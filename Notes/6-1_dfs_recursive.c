b51432b51594a65424f910bb6410ec93698f8148
Spencer Zhang <spencerpomme@live.com>
2015-12-06 20:25:51 +0800

    DEC06
    simplified

--- Notes/6-1_dfs_recursive.c
+++ Notes/6-1_dfs_recursive.c
@@ -5,22 +5,24 @@
 #include "LinkedGraph.h"  // organized graph ADT
 
 bool visited[MaxVertexNum];
-void dfs_recursive(Vertex v);
+void dfs_recursive(LGraph graph, Vertex v);
+
 int main(void)
 {
     LGraph graph = CreateGraph(MaxVertexNum);
-    dfs_recursive(0);
-
+    buildGraph_test(graph);
+    dfs_recursive(graph, 0);
+    //getchar();
     return 0;
 }
 
-void dfs_recursive(Vertex v)
+void dfs_recursive(LGraph graph, Vertex v)
 {
     /* depth first search of a graph beginning with vth vertex */
-    nodeptr w;
+    nodeptr ptr;
     visited[v] = true;
-    printf("%d ", v);
-    for (w = graph->G[v]; w; w = w->link)
-        if (!visited[wt->adjv])
-            dfs(w->adjv);
+    printf("visiting vertex: %d\n", v);
+    for (ptr = graph->G[v]; ptr; ptr = ptr->next)
+        if (!visited[ptr->adjv])
+            dfs_recursive(graph, ptr->adjv);
 }

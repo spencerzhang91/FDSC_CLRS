dc4480f237bffd08162faa343b12c2ca617bdf38
Spencer Zhang <spencerpomme@live.com>
2015-12-06 21:22:02 +0800

    DEC06
    modified breadth first search of adjacency based graph

---
+++ Notes/6-2_bfs_iterative.c
@@ -0,0 +1,31 @@
+/* The recursive implementation of depth first search of graph */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "LinkedGraph.h"  // organized graph ADT
+#include "LinkedQueue.h"  // organized queue ADT
+
+/* Modified definition of bfs: support function pointer as an argument */
+void bfs_iterative(LGraph graph, Vertex start, void (*func)(nodeptr p));
+void visit(nodeptr p);
+
+int main(void)
+{
+    int vertices = 10;
+    LGraph tg = CreateGraph(vertices);
+    buildGraph_test(tg);
+    showmatrix(tg);
+    bfs_iterative(tg, 0, visit);
+
+    return 0;
+}
+
+void bfs_iterative(LGraph graph, Vertex start, void (*func)(nodeptr p))
+{
+    
+}
+
+void visit(nodeptr p)
+{
+    printf("visiting vertext %d\n", p->adjv);
+}

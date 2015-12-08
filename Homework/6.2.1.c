31cc5cde246d41103d0b0874c879cb5c1077be4a
Spencer Zhang <spencerpomme@live.com>
2015-12-09 00:20:14 +0800

    DEC09
    using a flag to track if there is a unvisited vertex

--- Homework/6.2.1.c
+++ Homework/6.2.1.c
@@ -1,2 +1,40 @@
 /* excercise 6.2.1.c */
 /* implementation of dfs algorithm of adjacency matrix based graph */
+
+void dfs_iterative(MGraph graph, Vertex start, void (*func)(int p))
+{
+    int visited[graph->vertex_num];
+    Stack stack = CreateStack(MAX_STACK_SIZE);
+    int curr;
+    curr = start; // here curr marks current vertex
+    
+    (*func)(curr);
+    push(stack, curr);
+    visited[curr] = 1;
+    
+    while (curr != start || !IsEmpty(stack))
+    {
+        int flag = 0;
+        for (int i = 0; i < graph->vertex_num; i++)
+            if ((graph->G[curr][i] == 1) && (visited[i] != 1))
+            {
+                curr = i;
+                flag++;
+                break;
+            }
+        if (flag)
+        {
+            (*func)(curr);
+            visited[curr] = 1;
+            push(stack, curr);
+            display(stack); // check point
+        }    
+        else
+            curr = pop(stack);
+    }
+}
+
+void visit(int p)
+{
+    printf("visiting vertext %d\n", p);
+}

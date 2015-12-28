b6286f36734a0067ba1e55ac1285dbb24f2dc8a8
Spencer Zhang <spencerpomme@live.com>
2015-12-28 16:17:31 +0800

    DEC28

--- Notes/7-1_seqsearch.c
+++ Notes/7-1_seqsearch.c
@@ -1,20 +1,29 @@
 // The whole new start of FDSC after suspention due to essay preperation
 // code list 7-1: sequencial search
+#include <stdio.h>
 #define MAXSIZE 1000 // maximum item number plus one
 typedef struct {
     int key;
     /* other fields */
 } element;
 
-element list[MAXSIZE];
+element list[MAXSIZE] = {{1},{2},{3},{4},{5},{6},{7},{8}};
 
-int seqsearch(int list[], int target, int n)
+int seqsearch(element list[], int target, int n)
 {
     /* search an array, list, that has n numbers. Return i if list[i] = target
     Return -1 if target is not in the list. */
     int i;
-    list[n] = target;
-    for (i = 0; list[i] != target; i++)
+    list[n].key = target;
+    for (i = 0; list[i].key != target; i++)
         ;
     return ((i < n)? i: -1);
+}
+
+int main(void)
+{
+    int res = seqsearch(list, 47, 8);
+    printf("The result is %d\n", res);
+
+    return 0;
 }
--- Notes/7-2_binsearch.c
+++ Notes/7-2_binsearch.c
@@ -1,8 +1,34 @@
 // code list 7-2: binary search
+#include <stdio.h>
 #define MAXSIZE 1000 // maximum item number plus one
+#define COMPARE(x, y) ((x) > (y))? -1: (((x) == (y))? 0: 1)
 typedef struct {
     int key;
     /* other fields */
 } element;
 
-element list[MAXSIZE];
+element list[MAXSIZE] = {{1},{2},{3},{4},{5},{6},{7},{8}};
+
+int binsearch(element list[], int target, int n)
+{
+    int left = 0, right = n-1, middle;
+    while (left <= right)
+    {
+        middle = (left + right) / 2;
+        switch (COMPARE(target, list[middle].key))
+        {
+            case -1: left = middle + 1; break;
+            case 0: return middle;
+            case 1: right = middle - 1;
+        }
+    }
+    return -1;
+}
+
+int main(void)
+{
+    int res = binsearch(list, 45, 8);
+    printf("The result is %d\n", res);
+
+    return 0;
+}

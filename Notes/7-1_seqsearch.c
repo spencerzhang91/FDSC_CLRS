caeddbc51db36706f8264664a7de39235b320ca0
Spencer Zhang <spencerpomme@live.com>
2015-12-28 15:23:03 +0800

    DEC28

---
+++ Notes/7-1_seqsearch.c
@@ -0,0 +1,16 @@
+// The whole new start of FDSC after suspention due to essay preperation
+// code list 7-1: sequencial search
+#define MAXSIZE 1000 // maximum item number plus one
+typedef struct {
+    int key;
+    /* other fields */
+} element;
+
+element list[MAXSIZE];
+
+int seqsearch(int list[], int target, int n)
+{
+    /* search an array, list, that has n numbers. Return i if list[i] = target
+    Return -1 if target is not in the list. */
+
+}

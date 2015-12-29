907b8963ae8a1fbc68d51a442f0725ca35a1de7e
Spencer Zhang <spencerpomme@live.com>
2015-12-29 23:53:21 +0800

    DEC29

--- Homework/7.1.4.c
+++ Homework/7.1.4.c
@@ -4,13 +4,60 @@ search algorthms: binary search and interpolation search */
 
 #include <stdio.h>
 #define SIZE 5
+#define MAXSIZE 1000 // maximum item number plus one
+#define COMPARE(x, y) ((x) > (y))? -1: (((x) == (y))? 0: 1)
 typedef struct {
     int key;
     // any other possible fields
 } element;
 
+int itpsearch(element list[], int target, int n);
+int binsearch(element list[], int target, int n);
+void runtime(int (*func)(element list[], int len, int target));
+
 int main(void)
 {
+    element list[MAXSIZE] = {{1},{2},{3},{4},{5},{6},{7},{8}};
+    int res1 = itpsearch(list, 5, 8);
+    int res2 = binsearch(list, 5, 8);
+    printf("res1: %d  res2: %d\n", res1, res2);
 
     return 0;
+}
+
+int binsearch(element list[], int target, int n)
+{
+    int left = 0, right = n-1, middle;
+    while (left <= right)
+    {
+        middle = (left + right) / 2;
+        switch (COMPARE(target, list[middle].key))
+        {
+            case -1: left = middle + 1; break;
+            case 0: return middle;
+            case 1: right = middle - 1;
+        }
+    }
+    return -1;
+}
+
+int itpsearch(element list[], int target, int n)
+{
+    int left = 0;
+    int right = n - 1;
+    int mid;
+    while (list[left].key <= list[right].key && target >= list[left].key &&
+        target <= list[right].key)
+    {
+        mid = left + (target - list[left].key) *
+        ((right - left) / (list[right].key - list[left].key));
+        if (list[mid].key < target)
+            left = mid + 1;
+        else if (list[mid].key > target)
+            right = mid - 1;
+        else return mid;
+    }
+    if (target == list[left].key)
+        return left;
+    else return -1;
 }

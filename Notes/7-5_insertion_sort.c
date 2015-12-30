551507b0e918053fbf8166751d090fe016d29258
Spencer Zhang <spencerpomme@live.com>
2015-12-30 20:03:57 +0800

    DEC30

--- Notes/7-5_insertion_sort.c
+++ Notes/7-5_insertion_sort.c
@@ -1,4 +1,22 @@
 /* 7-5_InsertionSort.c */
+#include <stdio.h>
+#define SIZE 5
+
+typedef struct {
+    int key;
+    // any other possible fields
+} element;
+
+void insertion_sort(element list[], int n);
+
+int main(void)
+{
+    element list[SIZE] = {{5},{3},{2},{6},{1}};
+    insertion_sort(list, SIZE);
+
+    return 0;
+}
+
 void insertion_sort(element list[], int n)
 {
     // perform a insertion sort on the list

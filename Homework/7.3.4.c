3a353af8f690383cdbdfaf2aca9f016cb16bda77
Spencer Zhang <spencerpomme@live.com>
2016-01-06 10:44:26 +0800

    JAN06

--- Homework/7.3.4.c
+++ Homework/7.3.4.c
@@ -17,22 +17,15 @@ Linked insertion_sort(Linked list, bool (*func)(int x, int y));
 // basic operations of a linked list ADT
 Linked create(datatype value);
 Linked construct(datatype array[], int len);
-void destruct(Linked head, int len);
-void insert(Linked *head, Linked node, bool (*func)(int x, int y));
-void delitem(Linked *head, datatype item);
 void display(Linked head);
-int len(Linked head);
 
 int main(void)
 {
     Linked sortedhead;
     datatype list[SIZE] = {5, 3, 2, 6, 1, 8};
     Linked origin = construct(list, SIZE);
-    delitem(&origin, 2);
     display(origin);
-    delitem(&origin, 5);
-    display(origin);
-    /*
+    
     puts("befor sorting:");
     display(origin);
 
@@ -43,7 +36,7 @@ int main(void)
     puts("sort descendingly:");
     sortedhead = insertion_sort(origin, descending);
     display(sortedhead);
-    */
+
     return 0;
 }
 
@@ -59,20 +52,15 @@ bool descending(int x, int y)
 
 Linked insertion_sort(Linked list, bool (*func)(int x, int y)) // to be done
 {
-    /* perform a insertion sort on the linked list. Actually here's a little bit
-    detoured here, so rather slow. Will be rewritten later*/
-    int length = len(list);
-    if (length <= 1)
-        return list;
+    /* This function use the approach of create a new list rather than swap
+    order in place: which aquire three pointers and have sophisticated boundry
+    conditions. */
+    Linked curr = list->next;
     Linked newhead = create(list->key);
-    delitem(&list, list->key);
-    while (list)
+    while (curr)
     {
-        Linked curr = create(list->key);
-        insert(&newhead, curr, func);
-        delitem(&list, list->key);
+        
     }
-    return newhead;
 }
 
 Linked create(datatype value)
@@ -105,72 +93,6 @@ Linked construct(datatype array[], int len)
     return head;
 }
 
-void destruct(Linked head, int len)
-{
-    Linked temp = head;
-    Linked curr = temp->next;
-    while (curr)
-    {
-        free(temp);
-        temp = curr;
-        curr = temp->next;
-    }
-}
-
-void insert(Linked *head, Linked node, bool (*func)(int x, int y))
-{
-    // Insert node into the Linked list whose head is lead by 'head'
-    if (!head)
-    {
-        printf("Invalide insert position: %d.\n", pos);
-        exit(EXIT_FAILURE);
-    }
-    Linked pre = *head;
-    pre->next = *head;
-    if ((*func)((*head)->key, node->key))
-    {
-        node->next = *head;
-        *head = node;
-        return;
-    }
-    while (pre)
-    {
-        if ((*fun)(pre->key, node->key))
-        {
-            node->next = pre->next;
-            pre->next = node;
-            return;
-        }
-        else pre = pre->next;
-    }
-}
-
-void delitem(Linked *head, datatype item)
-{
-    // delete the requested item(if any)
-    Linked temp = *head;
-    if (temp->key == item)
-    {
-        (*head) = (*head)->next;
-        temp->next = NULL;
-        free(temp);
-    }
-    else
-    {
-        while (temp->next)
-        {
-            if (temp->next->key == item)
-                break;
-            else
-                temp = temp->next;
-        }
-        Linked drop = temp->next;
-        temp->next = temp->next->next;
-        drop->next = NULL;
-        free(drop);
-    }
-}
-
 void display(Linked head)
 {
     Linked temp = head;
@@ -180,16 +102,4 @@ void display(Linked head)
         temp = temp->next;
     }
     puts(""); 
-}
-
-int len(Linked head)
-{
-    int count = 0;
-    Linked temp = head;
-    while (temp)
-    {
-        count++;
-        temp = temp->next;
-    }
-    return count;
 }

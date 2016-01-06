3550ca4477ceb9ecc9747803151145b21b42f83a
Spencer Zhang <spencerpomme@live.com>
2016-01-06 23:39:34 +0800

    JAN06

--- Notes/7-6_quicksort_array.c
+++ Notes/7-6_quicksort_array.c
@@ -3,3 +3,11 @@ Actually I have to look up meterial about this algorithm in book DSAP to
 get more specific conduction. Array based implementation.
 */
 
+void quicksort(element list[], int left, int right)
+{
+    /* sort list[left], ..., list[right] into nondecreasing order on the
+    key field. list[left].key is arbitrarily chosen as the pivot key. It
+    is assumed that list[left].key <= lsit[right+1].key
+    */
+    
+}

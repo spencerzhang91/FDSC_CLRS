a4f2bc519bb1c13d30d4e4e8728b7baab8d607f5
Spencer Zhang <spencerpomme@live.com>
2016-01-06 23:00:14 +0800

    JAN06

--- Notes/7-6_quicksort_linked.c
+++ Notes/7-6_quicksort_linked.c
@@ -1,4 +1,6 @@
 /* 7-6_quicksort_linked.c
 Actually I have to look up meterial about this algorithm in book DSAP to 
-get more specific conduction. Linked list based implementation.
+get more specific conduction. 
+It's an implementation using linked list based queue, so that 
 */
+#include <stdio.h>

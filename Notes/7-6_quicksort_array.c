8dd2ddcc801e1cf023cc134019a0f69cba69055f
Spencer Zhang <spencerpomme@live.com>
2016-01-06 17:16:05 +0800

    JAN06

---
+++ Notes/7-6_quicksort_array.c
@@ -0,0 +1,4 @@
+/* 7-6_quicksort_array.c
+Actually I have to look up meterial about this algorithm in book DSAP to 
+get more specific conduction. Array based implementation.
+*/
---
+++ Notes/7-6_quicksort_linked.c
@@ -0,0 +1,4 @@
+/* 7-6_quicksort_linked.c
+Actually I have to look up meterial about this algorithm in book DSAP to 
+get more specific conduction. Linked list based implementation.
+*/

62b9575c7b157339f1cfdb3c30a38f1f59c0d929
Spencer Zhang <spencerpomme@live.com>
2016-01-07 17:27:10 +0800

    JAN07

--- Notes/7-6_quicksort_array.c
+++ Notes/7-6_quicksort_array.c
@@ -10,5 +10,22 @@ void quicksort(element list[], int left, int right)
     is assumed that list[left].key <= lsit[right+1].key
     */
     int pivot, i, j;
-    
+    element temp;
+    if (left < right)
+    {
+        i = left; j = right + 1;
+        pivot = list[left].key;
+        do {
+            /* search ofr keys from the left and right sublists, swapping
+            out-of-order elements until the left and right boundaries cross
+            or meet. */
+            do i++; while (list[i].key < pivot);
+            do j--; while (list[j].key > pivot);
+            if (i < j)
+                SWAP(list[i], list[j], temp);
+        } while (i < j);
+        SWAP(list[left], list[j], temp);
+        quicksort(list, left, j-1);
+        quicksort(list, j+1, right);
+    }
 }

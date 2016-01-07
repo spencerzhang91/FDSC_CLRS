b34ccddc2eee6c32b12b2be70d440c59f7a0c5e3
Spencer Zhang <spencerpomme@live.com>
2016-01-07 09:56:10 +0800

    JAN07

--- Notes/7-6_quicksort_linked.c
+++ Notes/7-6_quicksort_linked.c
@@ -4,9 +4,12 @@ get more specific conduction.
 It's an implementation using linked list based queue, so that 
 */
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "LinkedQueue.h"
 
-void quicksort(int *list, int left, int right);
+void quicksort(int array[]);
+static int array_len(int array[]);
 
 int main(void)
 {
@@ -14,7 +17,16 @@ int main(void)
     return 0;
 }
 
-void quicksort(int *list, int left, int right)
+void quicksort(int array[])
 {
-    
+    /* This implementation uses a queue ADT, very much alike 7-6_quicksort.py
+    It's also a recursive approach. */
+    if ((int length = array_len(array)) < 2)
+        return;
+    // devide
+    int pivot = array[0];
+    Queue L = CreateQueue(length / 2 + 1);
+    Queue E = CreateQueue(length / 2 + 1);
+    Queue G = CreateQueue(length / 2 + 1);
+    // yet done
 }

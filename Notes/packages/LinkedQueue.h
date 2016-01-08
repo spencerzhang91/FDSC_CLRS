a17c5d07177f14a08e897011e539b48d8f83bfc4
Spencer Zhang <spencerpomme@live.com>
2016-01-08 13:36:54 +0800

    JAN08

--- Notes/packages/LinkedQueue.h
+++ Notes/packages/LinkedQueue.h
@@ -2,7 +2,6 @@
 /* header files */
 #ifndef _LINKEDQUEUE_H
 #define _LINKEDQUEUE_H
-#include "LinkedGraph.h"
 #define ElementType int // can be changed according to particular purpose
 #define MAXSIZE 100
 
@@ -21,11 +20,13 @@ struct Qnode {
 };
 
 Queue CreateQueue(int maxsize);
+Queue ConstructQ(ElementType array[], int size);
 bool QIsFull(Queue Q);
 bool QIsEmpty(Queue Q);
 void enqueue(Queue Q, ElementType item);
 ElementType dequeue(Queue Q);
 ElementType first(Queue Q);
 int len(Queue Q);
+void displayQ(Queue Q);
 
 #endif

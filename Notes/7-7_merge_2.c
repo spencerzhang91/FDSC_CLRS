c31ce2fa5c57862c816508ef934815f54a38b4cc
Spencer Zhang <spencerpomme@live.com>
2016-01-14 14:26:44 +0800

    JAN14

---
+++ Notes/7-7_merge_2.c
@@ -0,0 +1,7 @@
+int mlist[] merge(int list1[], int list2[], int n, int m)
+{
+    /* merge two sorted lists list1 & list2 with length n and m, 
+    return a sorted merged mlist with length n+m
+    */
+    
+}

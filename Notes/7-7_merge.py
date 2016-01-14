590a72ff0fbf398e26eaa54927b82c9deeb4e064
Spencer Zhang <spencerpomme@live.com>
2016-01-14 13:45:12 +0800

    JAN14
    defined a counter to count how many steps this algorithm has taken

--- Notes/7-7_merge.py
+++ Notes/7-7_merge.py
@@ -1,4 +1,12 @@
 # merge sort algorithm on FDSC chap 7 page 213
+def counter():
+    init = 0
+    def add():
+        nonlocal init
+        init += 1
+        return init
+    return add
+
 def merge(list1, list2):
     """
     Input: two sorted lists of various length
@@ -8,19 +16,25 @@ def merge(list1, list2):
     len2 = len(list2)
     i = j = 0
     sorted_list = []
+    c = counter()
     while i < len1 and j < len2:
         if list1[i] <= list2[j]:
             sorted_list.append(list1[i])
             i += 1
+            c()
         else:
             sorted_list.append(list2[j])
             j += 1
+            c()
     while i < len1:
         sorted_list.append(list1[i])
         i += 1
+        c()
     while j < len2:
         sorted_list.append(list2[j])
         j += 1
+    res = c()
+    print(res)
     return sorted_list
 
 if __name__ == "__main__":
@@ -29,3 +43,5 @@ if __name__ == "__main__":
     L2 = [2,6,9,10,11]
     res = merge(L1, L2)
     print(res)
+    c = counter()
+    

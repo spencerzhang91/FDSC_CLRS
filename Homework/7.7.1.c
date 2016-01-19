0e6e887ddc11d99772eab4786ea44e1e6fd319dd
Spencer Zhang <spencerpomme@live.com>
2016-01-19 16:59:55 +0800

    JAN19

--- Homework/7.7.1.c
+++ Homework/7.7.1.c
@@ -1,1 +1,15 @@
 /* C implementation of non in-place heap sort algorithm, using MinHeap ADT */
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <stdbool.h>
+#include "MinHeap.h"
+void heapsort(int **array, int len)
+{
+    MinHeap heap = Create(len);
+    for (int i = 0; i < len; i++)
+        Insert(heap, (*array)[i]);
+    int j = 0;
+    while (!IsEmpty(heap) && j <= len)
+        (*array)[j++] = Delete(heap);
+}

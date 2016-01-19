6e9d4b42ead020d64444f1ee0005b2042ca06eec
Spencer Zhang <spencerpomme@live.com>
2016-01-19 20:11:51 +0800

    JAN19

--- Homework/7.7.1.c
+++ Homework/7.7.1.c
@@ -4,6 +4,20 @@
 #include <limits.h>
 #include <stdbool.h>
 #include "MinHeap.h"
+
+void heapsort(int **array, int len);
+void display(int *array, int len);
+
+int main(void)
+{
+    int numbers[10] = {1,22,3,7,100,53,17,65,8,9};
+    display(numbers, 10);
+    heapsort(&numbers, 10);
+    display(numbers, 10);
+
+    return 0;
+}
+
 void heapsort(int **array, int len)
 {
     MinHeap heap = Create(len);
@@ -12,4 +26,11 @@ void heapsort(int **array, int len)
     int j = 0;
     while (!IsEmpty(heap) && j <= len)
         (*array)[j++] = Delete(heap);
+}
+
+void display(int *array, int len)
+{
+    for (int i = 0; i < len; i++)
+        printf("%d ", array[i]);
+    puts("");
 }

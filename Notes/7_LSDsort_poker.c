7c560f5309ef8936132f34268201ec679fac6c49
Spencer Zhang <spencerpomme@live.com>
2016-01-23 21:05:04 +0800

    JAN23

--- Notes/7_LSDsort_poker.c
+++ Notes/7_LSDsort_poker.c
@@ -6,8 +6,9 @@
 #define SYMBOL_PART 2  // a card has graphic and numeric fields
 #define SUIT 4         // four suits
 #define FACE 13        // A2345678910JQK
+#define SIZE 8        // card number in hand
 
-enum suit {spade, heart, club, diamond};
+enum suit {S, H, C, D, X=10, J=11, Q=12, K=13};
 typedef struct card_node *Card;
 struct card_node {
     int suit[SUIT];
@@ -16,12 +17,24 @@ struct card_node {
 };
 
 Card radix_sort(Card ptr);
-void display(Card ptr);
+void display_list(Card ptr);
+void display_card(char (*poker)[2], int len);
 
 int main(void)
 {
-    int number[4] = {10,20,30,40};
-    printf("%d ", number[heart]);
+    char poker[SIZE][2] = {"SX","D9","C2","C1","HK","S4","D8","H3"};
+    display_card(poker, SIZE);
 
     return 0;
+}
+
+void display_card(char poker[][2], int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < 2; j++)
+            printf("%c", poker[i][j]);
+        printf(" ");
+    }
+    puts("");
 }

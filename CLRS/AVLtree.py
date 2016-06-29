b787de9737ccae36403ace5794b84846a39b9e7d
spencerpomme <spencerpomme@live.com>
2016-06-29 22:20:04 +0800

    JUN29
    folder modification

---
+++ CLRS/AVLtree.py
@@ -0,0 +1,7 @@
+class bstNode:
+    """
+    This class is representation of a node in BST.
+    Has left, right child and a parent pointer and a key value.
+    Also has a disconnect function.
+    """
+    
